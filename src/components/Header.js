adbbe05c384e8950c82f4da3b660d4843ca131cf
ThaliaRowan <thalia.rowan@gmail.com>
2021-02-10 15:43:40 -0400

    fixed navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,16 @@ function Header(){
     
     return(
         <div>
-           <nav class="navbar navbar-expand-lg ">
-      <div class="container">
+           <nav class="navbar navbar-dark navbar-expand-lg ">
+      <div className="container">
 
-        <a class="navbar-brand" href="index.html" id="logo">Thalia Romany</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        <a className="navbar-brand" href="index.html" id="logo">Thalia Romany</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
           aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
      
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
           <li className="nav-item">
         <Link to="/"  className="nav-link">
